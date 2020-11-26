d1ba0f4cb4d8a5d618043e89a559ec5e10e22770
FanJups <jupsfan@gmail.com>
2020-11-26 14:41:31 +0100

    Adding icons

--- src/main/java/com/mautourco/finance/App.java
+++ src/main/java/com/mautourco/finance/App.java
@@ -7,6 +7,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 /**
@@ -23,6 +24,7 @@ public class App extends Application {
 
 		primaryStage.setScene(new Scene(loadFXML("FinanceModule"), 1370, 700));
 		primaryStage.setTitle("Finance Module");
+		primaryStage.getIcons().add(new Image(App.class.getResourceAsStream("icons/mautourco2.jpg")));
 		primaryStage.show();
 	}
 
